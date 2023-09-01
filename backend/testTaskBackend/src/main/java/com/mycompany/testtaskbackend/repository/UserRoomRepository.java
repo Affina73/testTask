2540a76bf11dfe70bd9241a9746e0c2a71b03e6f
Мария Рябцева <affina73@gmail.com>
2023-09-01 22:42:49 +0200

    remove test

--- backend/testTaskBackend/src/main/java/com/mycompany/testtaskbackend/repository/UserRoomRepository.java
+++ backend/testTaskBackend/src/main/java/com/mycompany/testtaskbackend/repository/UserRoomRepository.java
@@ -3,6 +3,8 @@ package com.mycompany.testtaskbackend.repository;
 import com.mycompany.testtaskbackend.model.UserRecord;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRoomRepository extends MongoRepository<UserRecord, String> {
 }
